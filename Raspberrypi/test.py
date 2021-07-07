b76042f4efe5721d0d397c46cbc2a0c42f25a4c3
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2021-07-07 19:28:59 +0900

    あいうえお

---
+++ Raspberrypi/test.py
@@ -0,0 +1,17 @@
+import mysql.connector as mydb
+import time
+
+# コネクションの作成
+conn = mydb.connect(
+    host='localhost',
+    port='3306',
+    user='root',
+    password='root',
+    database='sousei_db'
+)
+cur = conn.cursor()
+start = time.time()
+cur.execute("select * from syusseki")
+a = cur.fetchall()
+print(time.time()-start)
+#print(a)
