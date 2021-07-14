e8bef33850758d4b3d14955771da2f502a83622e
suzuya21 <tyoppa21@gmail.com>
2021-07-14 18:28:29 +0900

    タイトルがうまくいかない

--- Raspberrypi/GUI/upload.py
+++ Raspberrypi/GUI/upload.py
@@ -5,27 +5,41 @@ from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from PySide2.QtCore import *
 #from tusin import post
+from shadow_effect import ShadowEffect
 
 
-# ウィンドウの見た目と各機能
+
 class uploadWindow(QWidget):
     def __init__(self, parent = None):
         super().__init__()
 
+        #ウィンドウ
+        self.setObjectName('uploadWindow')
+        self.setFixedSize(800,480)
+
+        #タイトル
         self.label = QLabel(self) 
         self.label.setText("<html>アップロード <img src='upload_icon.png'></html>")  
-        self.label.setFont(QFont('メイリオ',30))
-        self.label.setStyleSheet("Qlabel")
+        #タイトルの装飾
+        self.label.setFont(QFont('メイリオ',20))
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setObjectName('title')
+        self.label.setGraphicsEffect(ShadowEffect(self))
 
-        # コンボボックスオブジェクトの生成
+        # コンボボックスの生成+装飾
         self.kougi_combobox = QComboBox(self)
         self.kougi_combobox.setFont(QFont('メイリオ',20))
         self.kougi_combobox.setFixedSize(200,130)
-        #self.kougi_combobox.setStyleSheet("overflow:hidden; appearance:none; border-radius:30%; padding: 3px 15px 3px 15px;color:black;background-color: white;")
+        self.kougi_combobox.setObjectName('kougi_combobox')
+        self.kougi_combobox.setGraphicsEffect(ShadowEffect(self))
+        self.kougi_combobox.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Preferred)
 
         self.count_combobox = QComboBox(self)
         self.count_combobox.setFont(QFont('メイリオ',20))
         self.count_combobox.setFixedSize(200,130)
+        self.count_combobox.setObjectName('count_combobox')
+        self.count_combobox.setGraphicsEffect(ShadowEffect(self))
+        self.count_combobox.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Preferred)
 
         # 講義のコンボボックスの選択肢を追加
         self.kougi_combobox.addItems(['F1','F2','F3','F4','F5','M1','M2','M3','M4','M5'])
@@ -34,23 +48,42 @@ class uploadWindow(QWidget):
 
         #アップロードボタンを追加
         self.btn = QPushButton("ダウンロード")
+        #ファイル選択のダイアログが表示される
         self.btn.clicked.connect(self.getfile)
+
+        #コンボボックスから参照してファイルをアップロードする
         #self.btn.clicked.connect(self.upload_risyusya_list)
-        self.btn.setFixedSize(200,80)
-        btn_css = 'QPushButton{border-radius:20%;color:black;background-color: white;}QPushButton:hover{background-color: red;}'
-        self.btn.setStyleSheet(btn_css)
+
+        #アップロードボタンの装飾
+        self.btn.setFixedSize(300,80)
+        self.btn.setFont(QFont("メイリオ",20))
+        self.btn.setObjectName('btn')
+        self.btn.setGraphicsEffect(ShadowEffect(self))
+        self.btn.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Preferred)
 
         self.le = QLabel("Hello")
 
         #レイアウト
-        self.Ver_layout = QVBoxLayout()
-        self.Hol_layout = QHBoxLayout()
-        self.Hol_layout.addWidget(self.kougi_combobox)
-        self.Hol_layout.addWidget(self.count_combobox)
-        self.Ver_layout.addWidget(self.label)
-        self.Ver_layout.addLayout(self.Hol_layout)
-        self.Ver_layout.addWidget(self.btn)
-        self.setLayout(self.Ver_layout)
+        self.main_layout = QVBoxLayout()
+        self.main_layout = QGridLayout()
+        self.top_layout = QGridLayout()
+        self.middle_layout = QGridLayout()
+        self.bottom_layout = QGridLayout()
+        self.main_layout.setMargin(50)
+        self.main_layout.setSpacing(50)
+        self.top_layout.addWidget(QWidget(),0,0)
+        self.top_layout.addWidget(self.label,0,1,1,3)
+        self.top_layout.addWidget(QWidget(),0,4)
+        self.middle_layout.addWidget(self.kougi_combobox,0,0,1,2)
+        self.middle_layout.addWidget(QWidget(),0,2)
+        self.middle_layout.addWidget(self.count_combobox,0,3,1,2)
+        self.bottom_layout.addWidget(QWidget(),0,0)
+        self.bottom_layout.addWidget(self.btn,0,1,1,2)
+        self.bottom_layout.addWidget(QWidget(),0,3)
+        self.main_layout.addLayout(self.top_layout,0,0)
+        self.main_layout.addLayout(self.middle_layout,1,0,2,1)
+        self.main_layout.addLayout(self.bottom_layout,3,0)
+        self.setLayout(self.main_layout)
         self.setWindowTitle("出席管理-アップロード")
 
 
@@ -60,10 +93,10 @@ class uploadWindow(QWidget):
         fname = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\',"CSV files (*.csv)")
         self.le.setPixmap(QPixmap(fname))
 
-    """
+    """ #コンボボックスから参照してファイルをアップロードする
     def upload_risyusya_list(self):
         flag = True
-        post()
+        self.post()
 
         # ダウンロード成功時の処理
         if flag:
@@ -74,10 +107,11 @@ class uploadWindow(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = uploadWindow()
-
+    
+    #cssの読み込み
     with open('upload.css','r') as f:
-        style = f.read()
-        app.setStyleSheet(style)
+        css = f.read()
+        app.setStyleSheet(css)
 
     ex.show()
     sys.exit(app.exec_())
